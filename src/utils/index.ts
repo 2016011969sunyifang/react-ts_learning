23028be7d864826e5f2db00bafa204563cdbf953
Yifang Sun <2422617864@qq.com>
2021-08-05 15:52:35 +0800

    添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -47,7 +47,7 @@ export const useMount = (callback:()=> void)=>{
 //     log()#3 // 发现 timeout#2! 取消之，然后设置timeout#3
 //             // 所以，log()#3 结束后，就只剩timeout#3在独自等待了
 
-export const useDebounce = (value:unknown,delay?:number):any => {
+export const useDebounce = <V>(value:V,delay?:number):V => {
     console.log(value);
   const [debouncedValue, setDebouncedValue] = useState(value)
 
