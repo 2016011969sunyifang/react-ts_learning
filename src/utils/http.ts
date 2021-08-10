bd0cb77bbd18518c2f7f2157d34d08cc877031b6
Yifang Sun <2422617864@qq.com>
2021-08-10 16:29:35 +0800

    ts约束

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,7 @@
 import qs from "qs";
 import * as auth from "./../auth-provider";
 import { useAuth } from "./../context/auth-context";
-import { useCallback } from "react";
+// import { useCallback } from "react";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -58,38 +58,42 @@ export const useHttp = () => {
   return  (...[endpoint, config]: Parameters<typeof http>) => http(endpoint, { ...config, token: user?.token })
 };
 
-// 类型别名、Utility Type 讲解
-// 联合类型
-let myFavoriteNumber: string | number;
-myFavoriteNumber = "seven";
-myFavoriteNumber = 7;
-// TS2322: Type '{}' is not assignable to type 'string | number'.
-// myFavoriteNumber = {}
-let jackFavoriteNumber: string | number;
+// // 类型别名、Utility Type 讲解
+// // 联合类型
+// let myFavoriteNumber: string | number;
+// myFavoriteNumber = "seven";
+// myFavoriteNumber = 7;
+// // TS2322: Type '{}' is not assignable to type 'string | number'.
+// // myFavoriteNumber = {}
+// let jackFavoriteNumber: string | number;
 
-// 类型别名在很多情况下可以和interface互换
-// interface Person {
-//   name: string
-// }
-// type Person = { name: string }
-// const xiaoMing: Person = {name: 'xiaoming'}
+// // 类型别名在很多情况下可以和interface互换
+// // interface Person {
+// //   name: string
+// // }
+// // type Person = { name: string }
+// // const xiaoMing: Person = {name: 'xiaoming'}
 
-// 类型别名, interface 在这种情况下没法替代type
-type FavoriteNumber = string | number;
-let roseFavoriteNumber: FavoriteNumber = "6";
+// // 类型别名, interface 在这种情况下没法替代type
+// type FavoriteNumber = string | number;
+// let roseFavoriteNumber: FavoriteNumber = "6";
 
-// interface 也没法实现Utility type
-type Person = {
-  name: string;
-  age: number;
-};
-const xiaoMing: Partial<Person> = {};
-const shenMiRen: Omit<Person, "name" | "age"> = {};
-type PersonKeys = keyof Person;
-type PersonOnlyName = Pick<Person, "name" | "age">;
-type Age = Exclude<PersonKeys, "name">;
+// // interface 也没法实现Utility type
+// type Person = {
+//   name: string;
+//   age: number;
+// };
+// 不做约束但是可以有
+// const xiaoMing: Partial<Person> = {};
+// 删除无用的约束
+// const shenMiRen: Omit<Person, "name" | "age"> = {};
+// 遍历键值
+// type PersonKeys = keyof Person;
+// 获取子集
+// type PersonOnlyName = Pick<Person, "name" | "age">;
+// type Age = Exclude<PersonKeys, "name">;
 
-// Partial 的实现
-type Partial<T> = {
-  [P in keyof T]?: T[P];
-};
+// // Partial 的实现
+// type Partial<T> = {
+//   [P in keyof T]?: T[P];
+// };
