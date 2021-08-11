83b5b47d561658a989526b924789fba216a662ff
Yifang Sun <2422617864@qq.com>
2021-08-11 14:12:35 +0800

    封装styled布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,18 +1,47 @@
-import React from "react"
-import { useAuth } from "./context/auth-context"
-import { ProjectListScreen } from "./screens/project-list"
+import styled from "@emotion/styled";
+import React from "react";
+import { Row } from "./components/lib";
+import { useAuth } from "./context/auth-context";
+import { ProjectListScreen } from "./screens/project-list";
 
-export const AuthenticatedApp = ()=>{
-  const {logout} = useAuth();
-  return <div>
-    <button onClick={logout}>登出</button>
-    <ProjectListScreen/>
-  </div>
-}
+export const AuthenticatedApp = () => {
+  const { logout } = useAuth();
+  return (
+    <div>
+      <Container>
+        <Header between={true}>
+          <HeaderLeft  gap={true}>
+            <h3>logo</h3>
+            <h3>项目</h3>
+            <h3>用户</h3>
+          </HeaderLeft>
+          <HeaderRight>
+            <button onClick={logout}>登出</button>
+          </HeaderRight>
+        </Header>
+        <Main>
+          <ProjectListScreen />
+        </Main>
+      </Container>
+    </div>
+  );
+};
 
-// const PageHeader = style.header`
-// height:6rem;
-// `
-// const Main = styled.main`
-// height:calc(100vh-6rem);
-// `
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr ;
+  height: 100vh;
+`;
+const Main = styled.main`
+  display: flex;
+  overflow: hidden;
+  `;
+
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+const HeaderLeft = styled(Row)`
+`;
+const HeaderRight = styled.div``;
